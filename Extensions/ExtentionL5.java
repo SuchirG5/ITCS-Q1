baa84573918d710cac38f48c3b11c09418be9419
SuchirG5 <94651296+SuchirG5@users.noreply.github.com>
2022-05-26 20:32:27 -0400

    Add files via upload

---
+++ Extensions/ExtentionL5.java
@@ -0,0 +1,23 @@
+import edu.fcps.karel2.Display;
+import edu.fcps.karel2.Robot;
+
+public class ExtentionL5{
+
+public static void main(String args[]){
+
+
+   Display.openWorld("maps/loopChallenge.map ");
+   Robot bob = new Robot(1, 1, Display.NORTH, 0);
+
+      
+   
+   
+   
+   
+}
+
+
+
+
+
+}
